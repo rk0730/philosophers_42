5d41450e3639cf67c1a45738a7375627ae5550f0
Ryoma Kitao <rkitao@c1r4s4.42tokyo.jp>
2024-12-15 13:19:06 +0900

    param構造体　ft_arg.c追加

--- philo/philo.h
+++ philo/philo.h
@@ -6,19 +6,34 @@
 /*   By: rkitao <rkitao@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 23:22:48 by rkitao            #+#    #+#             */
-/*   Updated: 2024/12/15 00:19:16 by rkitao           ###   ########.fr       */
+/*   Updated: 2024/12/15 13:15:55 by rkitao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
-#define PHILO_H
+# define PHILO_H
 
-#include <stdio.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
 
-#ifdef RKITAO_DEBUG
-# define RKITAO(fmt, ...) printf(fmt, ##__VA_ARGS__)
-#else
-# define RKITAO(fmt, ...)
-#endif
+# ifdef RKITAO_DEBUG
+#  define RKITAO(fmt, ...) printf(fmt, ##__VA_ARGS__)
+# else
+#  define RKITAO(fmt, ...)
+# endif
+
+typedef struct s_param
+{
+	//引数からもらう情報
+	int	num_of_philo;
+	int	time_to_die;
+	int	time_to_eat;
+	int	time_to_sleep;
+	int	num_philo_must_eat;
+}		t_param;
+
+
+int	ft_arg(int argc, char **argv, t_param *param);
 
 #endif
