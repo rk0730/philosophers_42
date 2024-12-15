029dbc7d361f5e854cc9077e64df7cf477764eaa
rk0730 <164816796+rk0730@users.noreply.github.com>
2024-12-15 16:31:08 +0900

    common_dataの初期化

--- philo/ft_main.c
+++ philo/ft_main.c
@@ -6,11 +6,32 @@
 /*   By: rkitao <rkitao@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 23:32:12 by rkitao            #+#    #+#             */
-/*   Updated: 2024/12/15 16:22:14 by rkitao           ###   ########.fr       */
+/*   Updated: 2024/12/15 16:30:26 by rkitao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
+
+void	ft_init_common_data(t_common_data *data)
+{
+	int	i;
+
+	//　dataの初期化
+	data->forks = (pthread_mutex_t *)malloc(sizeof(pthread_mutex_t)
+			* data->num_of_philo);
+	data->eat_count = (int *)malloc(sizeof(int) * data->num_of_philo);
+	i = 0;
+	while (i < data->num_of_philo)
+	{
+		data->eat_count[i] = 0;
+		pthread_mutex_init(&data->forks[i], NULL);
+		i++;
+	}
+	data->dead = 0;
+	pthread_mutex_init(&data->lock_arg, NULL);
+	pthread_mutex_init(&data->lock_dead, NULL);
+	pthread_mutex_init(&data->lock_eat_count, NULL);
+}
 
 int	main(int argc, char **argv)
 {
@@ -19,7 +40,7 @@ int	main(int argc, char **argv)
 	data = (t_common_data *)malloc(sizeof(t_common_data));
 	RKITAO("debug mode\n");
 	if (ft_arg(argc, argv, data) == 1)
-		return (1);
-	
+		exit(EXIT_FAILURE);
+	ft_init_common_data(data);
 	return (0);
 }
