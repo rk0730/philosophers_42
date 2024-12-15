81c485c6c2c5abd03601c937b9aa47f811f14591
rk0730 <164816796+rk0730@users.noreply.github.com>
2024-12-15 13:19:06 +0900

    param構造体　ft_arg.c追加

--- philo/ft_arg.c
+++ philo/ft_arg.c
@@ -1,25 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_main.c                                          :+:      :+:    :+:   */
+/*   ft_arg.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rkitao <rkitao@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/14 23:32:12 by rkitao            #+#    #+#             */
-/*   Updated: 2024/12/15 13:16:17 by rkitao           ###   ########.fr       */
+/*   Created: 2024/12/15 13:09:48 by rkitao            #+#    #+#             */
+/*   Updated: 2024/12/15 13:18:10 by rkitao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int	main(int argc, char **argv)
+int	ft_arg(int argc, char **argv, t_param *param)
 {
-	t_param	*param;
-
-	param = (t_param *)malloc(sizeof(t_param));
-	(void)argv;
-	RKITAO("debug mode\n");
-	printf("start %d\n", argc);
-	ft_arg(argc, argv, param);
+	(void)argv;(void)param;
+	if (argc < 5 || argc > 6)
+	{
+		write(STDERR_FILENO,
+			"Usage: ./philo number_of_philosophers time_to_die time_to_eat "
+			"time_to_sleep [number_of_times_each_philosopher_must_eat]\n",
+			120);
+		return (1);
+	}
 	return (0);
 }
