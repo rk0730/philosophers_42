c99995591fe0f8944e42cf44eab0f92c1dabff2e
Ryoma Kitao <rkitao@c1r4s4.42tokyo.jp>
2024-12-15 16:15:52 +0900

    common data作成

--- philo/philo.h
+++ philo/philo.h
@@ -6,16 +6,17 @@
 /*   By: rkitao <rkitao@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 23:22:48 by rkitao            #+#    #+#             */
-/*   Updated: 2024/12/15 13:36:56 by rkitao           ###   ########.fr       */
+/*   Updated: 2024/12/15 16:13:21 by rkitao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
-# include <unistd.h>
+# include <pthread.h>
 # include <stdio.h>
 # include <stdlib.h>
+# include <unistd.h>
 
 # ifdef RKITAO_DEBUG
 #  define RKITAO(fmt, ...) printf(fmt, ##__VA_ARGS__)
@@ -23,22 +24,26 @@
 #  define RKITAO(fmt, ...)
 # endif
 
-typedef struct s_param
+typedef struct s_common_data
 {
 	//引数からもらう情報
-	int	num_of_philo;
-	int	time_to_die;
-	int	time_to_eat;
-	int	time_to_sleep;
-	int	num_philo_must_eat;
-}		t_param;
+	int				num_of_philo;
+	int				time_to_die;
+	int				time_to_eat;
+	int				time_to_sleep;
+	int				num_philo_must_eat;
+	pthread_mutex_t	*forks;
+	int				dead;
+	int				*eat_count;
+
+}					t_common_data;
 
 // utils
-int	ft_isdigit(int c);
-int	ft_strncmp(const char *s1, const char *s2, size_t n);
-int	ft_atoi(const char *str);
-int	ft_is_all_digit(char *str);
+int					ft_isdigit(int c);
+int					ft_strncmp(const char *s1, const char *s2, size_t n);
+int					ft_atoi(const char *str);
+int					ft_is_all_digit(char *str);
 // src
-int	ft_arg(int argc, char **argv, t_param *param);
+int					ft_arg(int argc, char **argv, t_common_data *param);
 
 #endif
