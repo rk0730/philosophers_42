aa81278178ce4ddba444b8ea1818fe052b3b7ea7
rk0730 <164816796+rk0730@users.noreply.github.com>
2024-12-15 13:56:33 +0900

    ft_is_all_digit atoiをint範囲に変更

--- philo/utils/ft_utils.c
+++ philo/utils/ft_utils.c
@@ -6,7 +6,7 @@
 /*   By: rkitao <rkitao@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/15 00:22:42 by rkitao            #+#    #+#             */
-/*   Updated: 2024/12/15 13:25:32 by rkitao           ###   ########.fr       */
+/*   Updated: 2024/12/15 13:56:07 by rkitao           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,16 @@ int	ft_isdigit(int c)
 		return (1);
 	else
 		return (0);
+}
+
+int	ft_is_all_digit(char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (str[i] && ft_isdigit(str[i]))
+		i++;
+	return (str[i] == '\0');
 }
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
@@ -43,17 +53,17 @@ static int	ft_atoi_helper(const char *start)
 	digit_len = 0;
 	while (ft_isdigit(start[digit_len]))
 		digit_len++;
-	if (digit_len > 19)
+	if (digit_len > 10)
 		return (1);
-	if (digit_len == 19 && ft_strncmp(start, "9223372036854775807", 19) > 0)
+	if (digit_len == 10 && ft_strncmp(start, "2147483647", 10) > 0)
 		return (1);
 	return (0);
 }
 
 int	ft_atoi(const char *str)
 {
-	int	sign;
-	int	result;
+	int sign;
+	int result;
 
 	sign = 1;
 	result = 0;
