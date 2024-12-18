fb7e867be495e56880436aaaee13ec93cee18454
北尾遼馬 <kitaoryoma@kitaoryoubanoMacBook-Air.local>
2024-12-18 18:19:29 +0900

    ft_is_dead作成

---
+++ philo/ft_monitor.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_monitor.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kitaoryoma <kitaoryoma@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/18 12:29:34 by kitaoryoma        #+#    #+#             */
+/*   Updated: 2024/12/18 16:56:42 by kitaoryoma       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "philo.h"
+
+// int	ft_monitor_dead(t_common_data *data)
+// {
+// 	int i;
+
+// 	pthread_mutex_lock(&data->lock_philo_data);
+// 	i = 0;
+// 	while ()
+
+// }
--- philo/philo.h
+++ philo/philo.h
@@ -6,7 +6,7 @@
 /*   By: kitaoryoma <kitaoryoma@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/14 23:22:48 by rkitao            #+#    #+#             */
-/*   Updated: 2024/12/18 17:42:13 by kitaoryoma       ###   ########.fr       */
+/*   Updated: 2024/12/18 18:18:58 by kitaoryoma       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ typedef struct s_args
 	int				time_to_eat;
 	int				time_to_sleep;
 	int				num_philo_must_eat;
-}			t_args;
+}					t_args;
 
 // 全員が共通で使うデータ
 typedef struct s_common_data
@@ -44,7 +44,7 @@ typedef struct s_common_data
 	// そのほか全員のt_philoのデータをlockする
 	pthread_mutex_t	lock_data;
 	struct timeval	start_time;
-	int				dead;
+	// int				dead;
 	int				*eat_count;
 	struct s_philo	*philos;
 	pthread_t		*threads;
@@ -60,7 +60,7 @@ typedef struct s_philo
 	pthread_mutex_t	*l_fork;
 	pthread_mutex_t	*r_fork;
 	int				*my_eat_count;
-	unsigned int	last_meal_time;
+	int				last_meal_time;
 }					t_philo;
 
 typedef enum e_message_type
@@ -90,7 +90,7 @@ int					ft_is_all_digit(char *str);
 // src
 int					ft_arg(int argc, char **argv, t_common_data *param);
 void				*ft_routine(void *arg);
-unsigned int		ft_get_time(t_common_data *data);
+int					ft_get_time(t_common_data *data);
 int					ft_is_finished(t_common_data *data);
 int					ft_get_args(t_common_data *data, t_data_type type);
 void				ft_message(t_philo *philo, t_message_type type);
