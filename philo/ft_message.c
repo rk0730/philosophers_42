37c003a94a0084be53057e663bccab58423522cc
rk0730 <164816796+rk0730@users.noreply.github.com>
2024-12-18 18:22:05 +0900

    ft_is_finished移動

--- philo/ft_message.c
+++ philo/ft_message.c
@@ -6,7 +6,7 @@
 /*   By: kitaoryoma <kitaoryoma@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/18 12:02:53 by kitaoryoma        #+#    #+#             */
-/*   Updated: 2024/12/18 18:18:20 by kitaoryoma       ###   ########.fr       */
+/*   Updated: 2024/12/18 18:20:38 by kitaoryoma       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,49 +46,6 @@ int	ft_get_time(t_common_data *data)
 	pthread_mutex_lock(&data->lock_data);
 	result = (now.tv_sec - data->start_time.tv_sec) * 1000 + (now.tv_usec
 			- data->start_time.tv_usec) / 1000;
-	pthread_mutex_unlock(&data->lock_data);
-	return (result);
-}
-
-static int	ft_is_dead(t_common_data *data)
-{
-	int	num_of_philo;
-	int	i;
-
-	num_of_philo = ft_get_args(data, NUM_OF_PHILO);
-	i = 0;
-	while (i < num_of_philo)
-	{
-		if (data->philos[i].last_meal_time
-			- ft_get_time(data) > ft_get_args(data, TIME_TO_DIE))
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-// 誰かが死んでいる、あるいは全員規定回数食べ切っているかどうか確認する
-int	ft_is_finished(t_common_data *data)
-{
-	int	result;
-	int	i;
-
-	result = 0;
-	pthread_mutex_lock(&data->lock_data);
-	if (ft_is_dead(data) > 0)
-		result = 1;
-	if (ft_get_args(data, NUM_PHILO_MUST_EAT) != -1)
-	{
-		i = 0;
-		while (i < data->args->num_of_philo)
-		{
-			if (data->eat_count[i] < data->args->num_philo_must_eat)
-				break ;
-			i++;
-		}
-		if (i == data->args->num_of_philo)
-			result = 1;
-	}
 	pthread_mutex_unlock(&data->lock_data);
 	return (result);
 }
