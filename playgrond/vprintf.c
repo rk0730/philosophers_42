9d9c428a8d00d2e8b21826630e40ab4f6e149e72
北尾遼馬 <kitaoryoma@kitaoryoubanoMacBook-Air.local>
2024-12-25 07:14:52 +0000

    printfのlockテスト

---
+++ playgrond/vprintf.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <stdarg.h>
+
+pthread_mutex_t		g_lock_print;
+
+void lock_print(const char *fmt, ...)
+{
+	pthread_mutex_lock(&g_lock_print);
+	va_list	ap;
+	va_start(ap, fmt);
+	vprintf(fmt, ap);
+	va_end(ap);
+	pthread_mutex_unlock(&g_lock_print);
+}
+
+void	*routine(void *arg)
+{
+	char *str = *(char **)arg;
+	lock_print("Hello, %s\n", str);
+	return (NULL);
+}
+
+int main(void)
+{
+	pthread_t thread;
+	char *str = "world";
+	pthread_mutex_init(&g_lock_print, NULL);
+
+	pthread_create(&thread, NULL, routine, &str);
+	lock_print("Hello, %s %d\n", "world", 1+1);
+
+	pthread_join(thread, NULL);
+	pthread_mutex_destroy(&g_lock_print);
+	return (0);
+}
